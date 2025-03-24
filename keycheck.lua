594c1c78cf749eefe8e12da96dc39780dfc474b0
PotatoTrollVn <65700247+PotatoTrollVn@users.noreply.github.com>
2025-03-24 17:10:59 +0700

    Update keycheck.lua

--- keycheck.lua
+++ keycheck.lua
@@ -1,1 +1,15 @@
+--! SERVER-VERIFIED SECTION !--
+local function verify(token, nonce)
+    local response = game:HttpGet(
+        "https://api.platoboost.com/check_script_token?"..
+        "token="..token.."&nonce="..nonce
+    )
+    return lDecode(response).valid
+end
+
+local token, nonce = ... -- From loader
+assert(verify(token, nonce), "Script verification failed"
+
+-- Only executes if both verifications passed
+-- MAIN SCRIPT CONTENT HERE !--
 print("Yes yes dop dop")
