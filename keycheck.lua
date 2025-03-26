add65f75e51ea5c1da7640b3ce2b319abc2b57b7
PotatoTrollVn <65700247+PotatoTrollVn@users.noreply.github.com>
2025-03-26 23:38:48 +0700

    Update keycheck.lua

--- keycheck.lua
+++ keycheck.lua
@@ -1,1 +1,3 @@
-print("Yes yes dop dop")
+if game.PlaceId == 914010731 then -- anomic 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PotatoTrollVn/super-duper-broccoli/refs/heads/main/Protected_9215433544528290.txt", true))()
+end
